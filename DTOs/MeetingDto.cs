6ce0e576d99a3f9933bc3f86b19a8f3c7710560f
ghena03 <ghenaobeidat7@gmail.com>
2026-04-30 22:13:07 +0300

    meetings DTO

---
+++ DTOs/MeetingDto.cs
@@ -0,0 +1,13 @@
+﻿namespace MOHRecognition.DTOs
+{
+    public class MeetingDto
+    {
+        public int Id { get; set; }
+
+        public int SessionNumber { get; set; }
+
+        public DateTime MeetingDate { get; set; }
+
+        public string? Notes { get; set; }
+    }
+}
