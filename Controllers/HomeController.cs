8c63b5372552ae676a805e8fee917e8a4af2b60f
kamelia9 <kmy2212189@ju.edu.jo>
2026-05-02 01:50:17 +0300

    last2may

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -7,7 +7,8 @@ using MOHRecognition.Services;
 using System.Diagnostics;
 using System.Text.Json;
 using System.Text.RegularExpressions;
-namespace MOHRecognition.Controllers 
+namespace MOHRecognition.Controllers  
+//2may
 {
    
     public class HomeController : Controller
